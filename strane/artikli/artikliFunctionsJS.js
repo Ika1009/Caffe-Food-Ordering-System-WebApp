7ec9e96cd9d6a5723e871ec9c8221a80d43de242
Ika1009 <ilija.doncic@yahoo.com>
2022-08-22 13:15:09 +0200

    lala

--- strane/artikli/artikliFunctionsJS.js
+++ strane/artikli/artikliFunctionsJS.js
@@ -316,7 +316,7 @@ document.querySelector("#dodajopciju").addEventListener("click", function (event
 });
 var add_artikl_pom = 0;
 
-document.querySelector("#artikl_form").addEventListener("submit", function (event) {
+document.querySelector("#popupDugme").addEventListener("submit", function (event) {
     event.preventDefault();
     // console.log("alo");
 
