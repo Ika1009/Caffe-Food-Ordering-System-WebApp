5073dfeee95ecc75c7020063cb6836b47d779bcd
Ika1009 <ilija.doncic@yahoo.com>
2022-10-28 12:30:55 +0200

    1

--- aplikacija/qr/strane/narudzbine/notifikacija.js
+++ aplikacija/qr/strane/narudzbine/notifikacija.js
@@ -1,17 +1,3 @@
-function notifikacija() {
-  console.log("LAGANO OBAVESENJCE ALEEE");
-  Notification.requestPermission().then((perm) => {
-    if (perm === "granted") {
-      const notification = new Notification("Imate nove narudzbine", {
-        body: "Proverite Let's order za nove narudzbine",
-        icon: "../../../slike/lets-order-logo.png",
-      });
 
-      notification.addEventListener("error", (e) => {
-        alert("error");
-      });
-    }
-  });
-}
 
 export {notifikacija};
